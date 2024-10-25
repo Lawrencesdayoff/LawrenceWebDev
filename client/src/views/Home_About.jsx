7a5349afe4497959a6f7debbbb51e1e5f875be7a
lawrencesdayoff <lawrencecarroll07@outlook.com>
2024-10-24 21:17:09 -0700

    modified:   client/src/App.css 	modified:   client/src/views/Home_About.jsx

--- client/src/views/Home_About.jsx
+++ client/src/views/Home_About.jsx
@@ -6,12 +6,14 @@ const Home_About = () => {
             
             <div class = "home_about_text">
                 <div>
-                    <p>My name is Lawrence Carroll, I am a software developer, an artist, and an organizer. <nl/>I am currently looking for any oppurtunity to allow myself to earn money as a programmer, so that I can sharpen my skills as I earn a living.</p>            
+                    <p>My name is Lawrence Carroll, I am a fullstack software developer, an artist, and an organizer. I am currently looking for any oppurtunity to allow myself to earn money as a programmer, so that I can sharpen my skills as I earn a living.</p>            
                     <p>I was born and raised in the Inland Empire in Southern California.</p>
                 </div>
-            <p> I studied Philosophy and Interdisciplinary Computing Arts at the University of California San Diego, graduating in 2021.</p>
-            <p> I spent 2 years working in the logistics industry as a warehouse worker, operating heavy equipment like standing reach forklifts, pretty cool stuff</p>
-            <p> I finished training as a fullstack software developer in 2024, certified by Colorado Technical University.</p>
+                <div>
+                    <p> I studied Philosophy and Interdisciplinary Computing Arts at the University of California San Diego, graduating in 2021.</p>
+                    <p> I spent 2 years working in the logistics industry as a warehouse worker, operating heavy equipment like standing reach forklifts, pretty cool stuff</p>
+                    <p> I finished training as a fullstack software developer in 2024, certified by Colorado Technical University.</p>
+                </div>
             <p> Currently looking for work as a software developer.</p>
             <br></br>
             <ul>Some hobbies of mine include:        
