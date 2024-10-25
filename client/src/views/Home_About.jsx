3fe7e77195ffb9a2c2be2b9f3f507ddba07de8e8
lawrence <lawrencecarroll07@outlook.com>
2024-10-24 18:10:26 -0700

    modified:   client/src/App.css 	modified:   client/src/index.css 	modified:   client/src/views/Home_About.jsx 	modified:   client/src/views/Home_SoftDev.jsx

--- client/src/views/Home_About.jsx
+++ client/src/views/Home_About.jsx
@@ -5,17 +5,20 @@ const Home_About = () => {
         <div class = "home_about">
             
             <div class = "home_about_text">
-            <p> My name is Lawrence, I was born and raised in the Inland Empire in Southern California.</p>
+                <div>
+                    <p>My name is Lawrence Carroll, I am a software developer, an artist, and an organizer. <nl/>I am currently looking for any oppurtunity to allow myself to earn money as a programmer, so that I can sharpen my skills as I earn a living.</p>            
+                    <p>I was born and raised in the Inland Empire in Southern California.</p>
+                </div>
             <p> I studied Philosophy and Interdisciplinary Computing Arts at the University of California San Diego, graduating in 2021.</p>
             <p> I spent 2 years working in the logistics industry as a warehouse worker, operating heavy equipment like standing reach forklifts, pretty cool stuff</p>
             <p> I finished training as a fullstack software developer in 2024, certified by Colorado Technical University.</p>
             <p> Currently looking for work as a software developer.</p>
-
-            <li>Some hobbies of mine include:</li>        
-            <ul>Strength training</ul>
-            <ul>Grappling</ul>
-            <ul>Community organizing</ul>
-            
+            <br></br>
+            <ul>Some hobbies of mine include:        
+            <li> Strength training</li>
+            <li> Grappling</li>
+            <li> Community organizing</li>
+            </ul>
             </div>
             
         </div>
--- client/src/views/Home_SoftDev.jsx
+++ client/src/views/Home_SoftDev.jsx
@@ -1,12 +1,11 @@
 const Home_SoftDev = () => {
     return(
         <>
-        <div className = "HomeBanner">
-        </div>
+   
         <div className="softwaredevelopment">
         <img src="/assets/github.png" height= "50px" width="50px"/>
         <a href ="https://github.com/Lawrencesdayoff" target = "_blank" > My Github </a>
-            <p> Here are some technologies I use on a regular basis in my programming:</p>
+            <p>I began my journey with programming and computer science late into my college career. I didn't think I had the gusto to hack it.</p>
             <div>
                 <img src="\assets\strike-export-1726307473129.png" height= "100px" width = "100px"></img>
                 <img src="\assets\strike-export-1726307493643.png" height= "100px" width = "100px"></img>
@@ -23,7 +22,7 @@ const Home_SoftDev = () => {
         <div className= "Project_List">
             <div className="card">
                 <a href="https://github.com/Lawrencesdayoff/TavernTextQuest-IdleRpg" className="project_link"> TavernQuest - Idle Role Playing Game</a>
-                <p> Still working on this one: A browser based idle role playing game. Imagine Oregon Trail with a little bit of NeoPets.</p>   
+                <p> Still working on this one: A browser based idle role playing game. Imagine Oregon Trail with a little bit of NeoPets. I am working to finish a decent version of it so I can port it to React Native and make it an application.</p>   
             </div>
             <div className="card">
                 <a href="https://github.com/Lawrencesdayoff/CodingDojo-Group-4-FinalGroupProject" className="project_link"> Booktok</a>
